9d5a23bed6690571b5263a33d5b9e7dc4c837543
Levi-LMN <Mukuhalevi@gmail.com>
2023-10-27 18:15:08 +0300

    Receiving input

---
+++ week7/input.py
@@ -0,0 +1,9 @@
+# Receiving Input
+inp_name = input('what is your name? ')
+print('Hi ' + inp_name)
+
+#exercise 2:ask 2 questions,name + color
+fi_name = input('what is your name? ')
+color = input('what is your favourite colour? ')
+print(fi_name + ' likes ' + color)
+
