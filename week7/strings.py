358d8d28f905e0325998879c45973dc0409a8bb0
Levi-LMN <Mukuhalevi@gmail.com>
2023-10-27 19:34:15 +0300

    strings and indexing

---
+++ week7/strings.py
@@ -0,0 +1,27 @@
+#strings
+
+##using a single quote brings errors when writing python's
+course = "Python's course for beginners"
+
+#adding a double quote in the middle
+course = 'Python course for "beginners"'
+
+##multi-line string
+course = '''
+Here is a 
+multi line
+string
+'''
+
+#indexing
+course = "Python's course for beginners"
+print(course[1])
+print(course[-1])
+print(course[0:3])
+print(course[1:])
+print(course[:5])
+print(course[:])
+
+name = 'talisman'
+print(name[1:-1])
+
