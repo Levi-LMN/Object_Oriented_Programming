0b077c3f4ec939b91c4cf75feaf2b8b66a897b57
Levi-LMN <Mukuhalevi@gmail.com>
2023-10-27 18:14:27 +0300

    Variables

---
+++ week7/variables.py
@@ -0,0 +1,16 @@
+print("Levi Mukuha")
+print('0----')
+print(' ||||')
+print('*' * 10)
+
+# Variables
+price = 10
+rating = 4.9
+name = 'Levi'
+is_published = False
+print(price)
+
+# Exercise
+name = 'John Smith'
+age = 20
+is_new = True
