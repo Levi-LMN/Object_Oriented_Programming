7e4ee717fad5c7b06001fd263c1ee3ba71513d2f
Levi-LMN <Mukuhalevi@gmail.com>
2023-10-27 18:39:26 +0300

    Type conversion

--- week7/conversion.py
+++ week7/conversion.py
@@ -14,3 +14,19 @@ print(weight_kg)
 temp_f = input('temperature in Farenheit? ')
 temp_c = float(temp_f) - 32 * 5/9
 print(temp_c)
+
+#Write a Python program that converts a number of seconds to hours, minutes, and remaining seconds. Prompt the user to enter a number of seconds, and then print out the equivalent time in hours, minutes, and seconds.
+
+# Prompt the user to enter a number of seconds
+seconds = int(input("Enter the number of seconds: "))
+
+# Calculate hours, minutes, and remaining seconds
+hours = seconds // 3600
+seconds %= 3600
+minutes = seconds // 60
+seconds %= 60
+
+# Print the equivalent time in hours, minutes, and seconds
+print(f"Equivalent time: {hours} hours, {minutes} minutes, and {seconds} seconds.")
+
+
