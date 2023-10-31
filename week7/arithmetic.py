7f61fe43509aee07fe4076a458f4c3663dfadb18
Levi-LMN <Mukuhalevi@gmail.com>
2023-10-31 19:58:10 +0300

    arithmetic

--- week7/arithmetic.py
+++ week7/arithmetic.py
@@ -1,1 +1,18 @@
+#arithmetic operations
+print(10 + 3) # addition
+print(10 / 3) # brings a float incl decimal
+print(10 // 3) # brings an integer only no decimal
+print(10 % 3) # modulo
+print(10 ** 3) # power
 
+x = 10
+x = x + 3 # adds 3
+x += 3 # adds 3 same as above
+x -= 3 # remove 3
+print(x)
+
+x = 10 + 3 * 2
+print(x)
+
+x = (2 + 3 ) * 10 - 3
+print(x)
---
+++ week7/even_or_odd.py
@@ -0,0 +1,5 @@
+y = int(input('enter number: '))
+if y % 2 == 0:
+    print("Even")
+else:
+    print("Odd")
