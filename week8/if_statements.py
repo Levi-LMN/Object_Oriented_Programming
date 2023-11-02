2cb2f641e5167d90bb0b25d6fb8cb56581a7b910
Levi-LMN <Mukuhalevi@gmail.com>
2023-11-02 11:47:50 +0300

    intro to if statements

---
+++ week8/if_statements.py
@@ -0,0 +1,9 @@
+ask = input('is it hot or cold? ')
+if ask == 'hot':
+    print("its a hot day")
+    print("Drink plenty of water")
+elif ask == 'cold' :
+    print("Its a cold day")
+    print("wear warm clothes")
+else:
+    print('Enjoy your day')
---
+++ week8/math.py
@@ -0,0 +1,11 @@
+import math
+
+x = 2.89
+print(round(x)) #round off
+print(abs(-x)) #absolute value
+
+print(math.ceil(2.9))
+print(math.floor(2.9))
+
+
+
