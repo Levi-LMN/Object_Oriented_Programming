f3a541727219a73087b723e09458c512ecbdb6b4
Levi Mukuha <117360388+Levi-LMN@users.noreply.github.com>
2023-12-15 01:47:31 +0300

    Keyword arguments

---
+++ recap/keyword_arguments.py
@@ -0,0 +1,10 @@
+# keyword arguments are arguments that are passed to a function by name
+
+
+def greet_user(first_name, last_name):
+    print(f"Hi {first_name} {last_name}")
+    print("welcome aboard")
+
+print("start")
+greet_user(last_name="levi", first_name="mukuha")
+print("finish")
