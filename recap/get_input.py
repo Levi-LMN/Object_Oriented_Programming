c6c1716c1cdff91939db0b5f3c9c6e0ff3e12281
Levi Mukuha <117360388+Levi-LMN@users.noreply.github.com>
2023-12-14 18:08:17 +0300

    input

---
+++ recap/get_input.py
@@ -0,0 +1,6 @@
+# ask two questions:persons name and favorite color
+# then print a message like "levi likes blue"
+
+name = input("what is your name? ")
+color = input("your favourite color? ")
+print('Hi ' + name, 'you like ' + color)
