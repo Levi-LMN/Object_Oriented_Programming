159168aaf540c96f09ea519a9d0cbb05e72d36ac
Levi Mukuha <117360388+Levi-LMN@users.noreply.github.com>
2023-12-14 23:07:44 +0300

    while loop

---
+++ recap/while_loop.py
@@ -0,0 +1,20 @@
+# while loops
+# while loops are used to execute a block of code multiple times
+
+# Syntax:
+# while condition:
+#     statements
+
+# Example:
+i = 1
+
+while i <= 5:
+    print(i)
+    i += 1
+print("done")
+
+i = 1
+while i <= 5:
+    print('*' * i)
+    i += 1
+print("done")
