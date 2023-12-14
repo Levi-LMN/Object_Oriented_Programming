3f60a584fc48a617fde4636acb9a0eef207b2ba9
Levi Mukuha <117360388+Levi-LMN@users.noreply.github.com>
2023-12-14 23:31:27 +0300

    for loop

---
+++ recap/for_loops.py
@@ -0,0 +1,11 @@
+for item in range(5 , 10, 2):
+    print(item)
+
+# prices in shoppping cart
+prices = [10, 20, 30]
+total = 0
+
+for item in prices:
+    total += item
+    print(f"the total is {total}")
+
