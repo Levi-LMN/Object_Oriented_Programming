94df8e590b40d8e6ca17db4416e912c1ff67beb8
Levi Mukuha <117360388+Levi-LMN@users.noreply.github.com>
2023-12-15 01:40:27 +0300

    parameters

---
+++ recap/parameters.py
@@ -0,0 +1,9 @@
+# parameters are the input that we give to a function
+
+def greet_user(first_name, last_name):
+    print(f"Hi {first_name} {last_name}")
+    print("welcome aboard")
+
+print("start")
+greet_user("levi", "mukuha")
+print("finish")
