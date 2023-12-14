1cd3d18eeb8f1de007674eadf5bbed2adc222a18
Levi Mukuha <117360388+Levi-LMN@users.noreply.github.com>
2023-12-14 18:27:49 +0300

    type conversion eg from str to int

---
+++ recap/type_conversion.py
@@ -0,0 +1,12 @@
+birth_year = int(input('birth year: '))
+age = 2023 - birth_year
+print(f'you are {age} years old')
+
+print(type(age))
+
+#ask user their weight in pounds then convert to kilograms
+# print on terminal the result
+
+weight_pounds = int(input('what is your weight in pounds? '))
+weight_kg = weight_pounds * 0.45
+print(weight_kg)
