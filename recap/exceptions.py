1d33a47001c46072575f6356fbbe5e49c7852c0e
Levi Mukuha <117360388+Levi-LMN@users.noreply.github.com>
2023-12-15 02:07:12 +0300

    error handling

---
+++ recap/exceptions.py
@@ -0,0 +1,12 @@
+# error handling
+
+try:
+    age = int(input("Age: "))
+    income = 20000
+    risk = income / age
+    print(age)
+except ZeroDivisionError:
+    print("Age cannot be zero")
+
+except ValueError:
+    print("Invalid value")
