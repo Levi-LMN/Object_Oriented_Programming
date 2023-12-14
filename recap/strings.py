c70f601578737a179ccdd14538a182e8ddd09e54
Levi Mukuha <117360388+Levi-LMN@users.noreply.github.com>
2023-12-14 18:55:43 +0300

    strings and []

---
+++ recap/strings.py
@@ -0,0 +1,42 @@
+course = "python's course for beginnners"
+print(course)
+
+course = 'python for "beginnners"'
+print(course)
+
+course = '''
+Hi John,
+
+Here is our first email
+
+Thankyou
+'''
+print(course)
+
+course = 'python for beginnners'
+print(course[0])
+
+course = 'python for beginnners'
+print(course[-1])
+
+course = 'python for beginnners'
+print(course[0:3])
+
+course = 'python for beginnners'
+print(course[:5])
+
+course = 'python for beginnners'
+print(course[:])
+
+course = 'python for beginnners'
+another = course[:]
+print(course)
+print(another)
+
+
+
+
+# define name as jeniffer and print
+
+name = 'Jeniffer'
+print(name[1:-1])
