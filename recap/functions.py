5e584c1366675274642762aa8107f32bd0142ee3
Levi Mukuha <117360388+Levi-LMN@users.noreply.github.com>
2023-12-15 01:33:24 +0300

    functions

---
+++ recap/functions.py
@@ -0,0 +1,16 @@
+# functions in python refer to a block of code that is used to perform a single task
+# functions are reusable
+# functions are used to organize code
+# functions are defined using the def keyword
+# functions are called using the function name
+# functions can have parameters
+# functions can return values
+
+def greet_user():
+    print("Hi there")
+    print("welcome aboard")
+
+print("start")
+greet_user()
+print("finish")
+
