adda5926089cd85aa76844d9fe65b8d7156daa73
Levi Mukuha <117360388+Levi-LMN@users.noreply.github.com>
2023-12-14 20:24:47 +0300

    arithmetic operations

---
+++ recap/arithmetic_operations.py
@@ -0,0 +1,16 @@
+print(10 / 3) # division
+print(10 // 3) # floor division
+print(10 % 3) # modulus operator
+print(10 ** 3) # exponent operator
+x = 10
+x = x + 3
+print(x)
+
+# augmented assignment operator
+x = 10
+x += 3
+print(x)
+
+# operator precedence
+x = 10 + 3 * 2
+print(x)
