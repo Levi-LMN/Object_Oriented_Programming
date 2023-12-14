f65f967c2a6c3cbcf9c7528bb07cc7bf52b50f9f
Levi Mukuha <117360388+Levi-LMN@users.noreply.github.com>
2023-12-14 20:28:01 +0300

    math functions

---
+++ recap/math_functions.py
@@ -0,0 +1,17 @@
+# round function rounds the number to the nearest integer
+x = 2.9
+print(round(x))
+
+# abs function returns the absolute value of a number
+x = -2.9
+print(abs(x))
+
+# math module
+import math
+
+# ceil function returns the smallest integer greater than or equal to a number
+print(math.ceil(2.9))
+
+# floor function returns the largest integer less than or equal to a number
+print(math.floor(2.9))
+
