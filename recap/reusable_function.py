c42404260aecee86527ec16a21da605b3bdc7ba6
Levi Mukuha <117360388+Levi-LMN@users.noreply.github.com>
2023-12-15 01:59:32 +0300

    reusable function

---
+++ recap/reusable_function.py
@@ -0,0 +1,15 @@
+# reusable emoji converter
+
+def emoji_converter(message):
+    words = message.split(" ")
+    emojis = {
+        ":)": "😀",
+        ":(": "😞"
+    }
+    output = ""
+    for word in words:
+        output += emojis.get(word, word) + " "
+    return output
+
+message = input(">")
+print(emoji_converter(message))
