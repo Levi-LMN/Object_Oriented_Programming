12bca68cf5d89bd4a73936ea5690a240f92a5638
Levi Mukuha <117360388+Levi-LMN@users.noreply.github.com>
2023-12-14 18:00:54 +0300

    variables

---
+++ recap/variables.py
@@ -0,0 +1,9 @@
+# Variables
+# Variables are used to store data in memory
+# Variables are created when you assign a value to it
+# We check in a patient named John Smith. He's 20 years old and is a new patient.
+# define three variables to store these values
+
+name = 'John Smith'
+age = 20
+is_new = True
