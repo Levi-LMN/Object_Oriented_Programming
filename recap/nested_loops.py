2c81131cace5195d130444bc32f8ab3411de7ce7
Levi Mukuha <117360388+Levi-LMN@users.noreply.github.com>
2023-12-14 23:59:32 +0300

    nested loop

---
+++ recap/nested_loops.py
@@ -0,0 +1,11 @@
+# how to use nested loops
+
+for x in range(4):
+    for y in range(3):
+        print(f'({x} , {y})')
+
+# exercise
+numbers = [5, 2, 5, 2, 2]
+
+for number in numbers:
+    print(f"*" * number)
